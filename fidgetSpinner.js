415532ae266d1f2613e546b6665282de53920e43
Yisp <youjustdoxxedme@gmail.com>
2017-06-30 11:27:38 -0500

    Initial commit

---
+++ fidgetSpinner.js
@@ -0,0 +1,73 @@
+var fidget;
+var fidgetSpinners = [];
+
+var spinFrame = 0;
+var totalSpins = 0;
+
+function fidgetSpinner() {
+
+	this.spin = function(spinFrame) {
+
+		this.x = mouseX;
+		this.y = mouseY;
+
+		if (this.y <= 0) { this.y = 0; }
+		if (this.y >= height) { this.y = height; }
+
+		if (this.x <= 0) { this.x = 0; }
+		if (this.x >= width) { this.x = width; }
+
+		imageMode(CENTER);
+		image(fidgetSpinners[spinFrame], this.x, this.y, 250, 250);
+
+	}
+
+}
+
+function preload() {
+
+	song = loadSound("Sounds/fidgetSpinnerSong.mp3");
+
+	for (var i = 1; i <= 4; i++) {
+
+		fidgetSpinners.push(loadImage("Spinners/fidgetSpin" + i + ".png"));
+
+	}
+
+}
+
+function setup() {
+
+	fidget = new fidgetSpinner();
+
+	createCanvas(500, 500);
+	background(0, 0, 255);
+
+}
+
+function draw() {
+
+	if (!song.isPlaying()) {
+
+		song.play();
+
+	}
+
+	background(0, 0, 255);
+	
+	textSize(24);
+	text("Total Spins: " + totalSpins, 2, 475);
+	
+	frameRate(20);
+
+	fidget.spin(spinFrame);
+	spinFrame++;
+
+	if (spinFrame == fidgetSpinners.length) {
+
+		spinFrame = 0;
+		totalSpins++;
+
+	}
+
+}
